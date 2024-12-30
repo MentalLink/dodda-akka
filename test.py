882f620c5e1163e1ac272adaafba32db7bcefffa
Pranav Sridhar <63054696+MentalLink@users.noreply.github.com>
2024-12-30 11:31:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 import google.generativeai as genai
 
-genai.configure(api_key='AIzaSyBco7C-oBK-6mx6qYIznGrTw4z9Ky2NdYk')
+genai.configure(api_key='API-KEY')
 
 model = genai.GenerativeModel('gemini-2.0-flash-exp')
 
